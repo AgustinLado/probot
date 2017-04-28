03da5ca7deef2ba781de159e82ecb0f036122238
Agustín Lado <AgustinLado@gmail.com>
2017-04-28 16:40:13 -0300

    (keep-defender): Stupid basic bot. Click the buttons 100 times per second :P
    Record already broken!

--- keep_defenders.py
+++ keep_defenders.py
@@ -1,4 +1,23 @@
 import pyautogui
 
-pyautogui.typewrite('Hello world!')
-pyautogui.click()
+
+class DefendTheKeep(object):
+    """
+    Play Keep Defenders by Marccio Silva.
+    https://marcciosilva.itch.io/keep-defenders
+
+    Just click the damn buttons!
+    """
+    def defend(self):
+        pyautogui.PAUSE = 0.01
+        while True:
+            pyautogui.moveTo(860, 250)
+            pyautogui.click()
+            pyautogui.moveTo(960, 250)
+            pyautogui.click()
+            pyautogui.moveTo(1060, 250)
+            pyautogui.click()
+
+
+defender = DefendTheKeep()
+defender.defend()
